cb3679e1394b451f654b5f6c2196a8df79a98109
Joseph Mearman <joseph@mearman.co.uk>
2023-12-07 11:02:05 +0000

    Add CLI test for no params

--- seeds/breadboard-cli-tools/tests/index.ts
+++ seeds/breadboard-cli-tools/tests/index.ts
@@ -5,7 +5,29 @@
  */
 
 import test from "ava";
+import { exec } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
 
-test("placeholder", async (t) => {
-  t.pass()
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const cliPath = path.resolve(path.join(__dirname, "../src/index.js"));
+
+function execCli(args = ""): Promise<unknown> {
+  return new Promise((resolve) => {
+    exec(`node "${cliPath}" ${args}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(error);
+        resolve(stderr);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+test("calling CLI with no parameters shows usage text", async (t) => {
+  const output = (await execCli()) as string;
+  console.debug(output);
+  const expected = "Usage: index [options] [command]";
+  t.true(output.includes(expected));
 });

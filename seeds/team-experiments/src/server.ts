f474d1fceafebc13b151082686ab260563320c50
Dimitri Glazkov <dglazkov@google.com>
2024-04-15 15:49:13 -0700

    Add a simple server setup.

---
+++ seeds/team-experiments/src/server.ts
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { readFile } from "fs/promises";
+import http from "http";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createServer as createViteServer } from "vite";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const vite = await createViteServer({
+  server: { middlewareMode: true },
+  appType: "custom",
+});
+
+const serveIndex = async (
+  req: http.IncomingMessage,
+  res: http.ServerResponse
+) => {
+  const url = req.url;
+  if (!url) {
+    // Don't know how to serve this, let's bail.
+    return;
+  }
+
+  if (url !== "/" && url !== "/index.html") {
+    res.writeHead(404, "Page not found");
+    res.end("Page Not Found. Are you looking for '/index.html' maybe?");
+    return;
+  }
+
+  const index = await readFile(
+    path.resolve(__dirname, "../index.html"),
+    "utf-8"
+  );
+  const transformed = await vite.transformIndexHtml("/index.html", index, url);
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(transformed);
+};
+
+const server = http.createServer(async (req, res) => {
+  vite.middlewares(req, res, async () => {
+    await serveIndex(req, res);
+  });
+});
+
+console.info("Serving from port 3000...");
+server.listen(3000);

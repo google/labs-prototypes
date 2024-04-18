36a13c4448a225f3c228c15ba754d0e08b127aa5
Dimitri Glazkov <dglazkov@google.com>
2024-04-18 14:43:05 -0700

    Introduce `get page` tool.

--- seeds/team-experiments/src/server/browse.ts
+++ seeds/team-experiments/src/server/browse.ts
@@ -9,13 +9,18 @@ import puppeteer from "puppeteer";
 export const getPageContent = async (url: string) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
-  await page.setViewport({ width: 1080, height: 1024 });
-  // TODO: Do something more elaborate.
-  const content = await page.evaluate(() => {
-    return document.body.innerText;
-  });
+  try {
+    await page.goto(url);
+    await page.setViewport({ width: 1080, height: 1024 });
+    // TODO: Do something more elaborate.
+    const content = await page.evaluate(() => {
+      return document.body.innerText;
+    });
 
-  await browser.close();
-  return content;
+    return content;
+  } catch (e) {
+    return `Unable to retrieve the page: ${(e as Error).message}`;
+  } finally {
+    await browser.close();
+  }
 };
